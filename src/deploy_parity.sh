49385dc4493fe2e82a4848e40b5f9e9cb4970df6
Cloudflying <oss@live.hk>
2019-04-15 13:27:23 +0800

    Add parity Deploy Script

---
+++ src/deploy_parity.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+# Onekey Install Parity (Ethereum Wallet)
+
+saveDir='/opt/parity'
+dataDir=${saveDir}/'data'
+
+if [[ ! -d ${saveDir} ]]; then
+	sudo mkdir -p ${saveDir}
+fi
+
+if [[ ! -d ${dataDir} ]]; then
+	sudo mkdir -p ${dataDir}
+fi
+
+function latest_version()
+{
+	VERSION=$(curl -sI https://github.com/paritytech/parity-ethereum/releases/latest | grep Location | awk -F '/v' '{print $2}')
+	echo ${VERSION}
+}
+
+function get_download_url()
+{
+		# URL="https://releases.parity.io/ethereum/v$(latest_version)/x86_64-unknown-linux-gnu/parity"
+		URL=$(curl -sL https://github.com/paritytech/parity-ethereum/releases/latest | grep x86_64-unknown-linux-gnu/parity | awk -F '"' '{print $4}')
+		echo ${URL}
+}
+
+# latest_version
+# get_download_url
+
+function install_parity()
+{
+	echo "Current Version" $(latest_version)
+	echo "=> Downloading Parity ... "
+	if [[ -f '/opt/parity/parity' ]]; then
+		echo "look like has been installed, skip it"
+	else
+		sudo wget $(get_download_url) -O /opt/parity/parity
+		sudo chmod +x parity
+	fi
+	echo "Install Success , start help with /opt/parity/parity -h "
+}
+
+function reinstall_parity()
+{
+	echo "Current Version" $(latest_version)
+	echo "=> Downloading Parity ... "
+	if [[ -f '/opt/parity/parity' ]]; then
+		sudo rm -fr /opt/parity/parity
+		sudo wget $(get_download_url) -O /opt/parity/parity
+		sudo chmod +x parity
+	else
+		echo "Parity Look like Not Install ,skip it"
+	fi
+	echo "Install Success , start help with /opt/parity/parity -h "
+}
+
+function upgrade_parity()
+{
+	echo "Current Version" $(latest_version)
+	echo "=> Downloading Parity ... "
+	if [[ -f '/opt/parity/parity' ]]; then
+		sudo rm -fr /opt/parity/parity
+		sudo wget $(get_download_url) -O /opt/parity/parity
+		sudo chmod +x parity
+	else
+		echo "Parity Look like Not Install ,skip it"
+	fi
+	echo "Install Success , start help with /opt/parity/parity -h "
+}
+
+case $1 in
+	version | -v )
+		/opt/parity/parity -v | grep version\ Parity | awk -F ' ' '{print $2}'
+		;;
+	latest-version )
+		latest_version
+		;;
+	install )
+		install_parity
+		;;
+	reinstall )
+		reinstall_parity
+		;;
+	upgrade )
+		upgrade_parity
+		;;
+	* )
+		echo "Command usage: install |  "
+		;;
+esac
